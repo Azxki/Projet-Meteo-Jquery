4f63227e70b591d8bdd89049a49152d34b3e444c
azxki <cococoursan@gmail.com>
2022-12-22 11:28:32 +0100

    exo

--- app.js
+++ app.js
@@ -1,47 +1,25 @@
-let ville = document.getElementById('ville');
-let date = document.getElementById('date');
-let degres = document.getElementById('degres');
-let button = document.getElementById('button');
-let input = document.getElementById('input');
-let humidity = document.getElementById('humidite');
+let ville = $('#ville');
+let date = $('#date');
+let degres = $('#degres');
+let button = $('#button');
+let input = $('#input');
+let humidity = $('#humidite');
 
-let aujourdhui = new Date();
-date.innerHTML = aujourdhui.toLocaleDateString();
+let now = new Date();
+
+let aujourdhui = now.getDate() + "/" + now.getMonth() + "/" + now.getFullYear();
+date.html(aujourdhui);
 
 
 let printPosition = function (coordinates) {
     console.log(coordinates)
 
     let requestURL =
-        "https://api.openweathermap.org/data/2.5/weather?lat=" + `${coordinates.coords.latitude}` + "&lon=" + `${coordinates.coords.longitude}` + "&lang=fr&appid=8a4a001f1348b4bd88e32c9272eb8994&units=metric";
+        "https://api.openweathermap.org/data/2.5/weather?lat=" + `${coordinates.coords.latitude}` + "&lon=" +
+        `${coordinates.coords.longitude}` + "&lang=fr&appid=8a4a001f1348b4bd88e32c9272eb8994&units=metric";
 
-    button.addEventListener('click', function () {
-        requestURL =
-            "https://api.openweathermap.org/data/2.5/weather?q=" + input.value + "&appid=8a4a001f1348b4bd88e32c9272eb8994&lang=fr&units=metric";
+    let xhr = new XMLHttpRequest();
 
-        let xhr = new XMLHttpRequest();
-
-        xhr.open("GET", requestURL);
-        xhr.responseType = "json";
-
-        xhr.onload = function () {
-            if (xhr.status !== 200) {
-                alert("Une erreur est survenue !");
-                return;
-            }
-
-            let response = xhr.response;
-            console.log(response);
-            ville.innerHTML = response.name + ", " + response.sys.country;
-            degres.innerHTML = "Température " + Math.trunc(response.main.temp) + "°C <br> Température max : " +
-                Math.trunc(response.main.temp_max) + "°C <br> Température min : " + Math.trunc(response.main.temp_min) + "°C";
-            degres.style.fontSize = "130%";
-            humidity.innerHTML = "Humidité : " + Math.trunc(response.main.humidity) + "%";
-            humidity.style.fontSize = "110%";
-        }
-
-        xhr.send();
-    })
     xhr = new XMLHttpRequest();
 
     xhr.open("GET", requestURL);
@@ -55,12 +33,12 @@ let printPosition = function (coordinates) {
 
         let response = xhr.response;
         console.log(response);
-        ville.innerHTML = response.name + ", " + response.sys.country;
-        degres.innerHTML = "Température " + Math.trunc(response.main.temp) + "°C <br> Température max : " +
-            Math.trunc(response.main.temp_max) + "°C <br> Température min : " + Math.trunc(response.main.temp_min) + "°C";
-        degres.style.fontSize = "130%";
-        humidity.innerHTML = "Humidité : " + Math.trunc(response.main.humidity) + "%";
-        humidity.style.fontSize = "110%";
+        ville.html(response.name + ", " + response.sys.country);
+        degres.html("Température " + Math.trunc(response.main.temp) + "°C <br> Température max : " +
+            Math.trunc(response.main.temp_max) + "°C <br> Température min : " + Math.trunc(response.main.temp_min) + "°C");
+        degres.addClass('up2');
+        humidity.html("Humidité : " + Math.trunc(response.main.humidity) + "%");
+        humidity.addClass('up');
     }
 
     xhr.send();
